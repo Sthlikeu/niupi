1c76c251391b643a107102b13bc3f78e878a14fa
Sthlikeu <2581637757@qq.com>
2024-05-14 18:56:02 +0800

    1

---
+++ new.java
@@ -0,0 +1,17 @@
+
+public class ¾Å¾Å³Ë·¨±í {
+
+	public static void main(String[] args) {
+		for(int i=1;i<10;i++)
+		{
+			for(int j=1;j<10;j++)
+			{
+				int k=i*j;
+				System.out.print(" "+i+"*"+j+"="+k);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
